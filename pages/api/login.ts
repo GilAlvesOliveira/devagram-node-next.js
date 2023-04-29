864311202bdf031358a6fcd1cb673ec1be25f3b0
Ana Cristina oliveira <121919367+GilAlvesOliveira@users.noreply.github.com>
2023-04-29 20:19:47 -0300

    conclusao do jwt

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,9 +1,9 @@
-import type { NextApiRequest, NextApiResponse } from 'next';
-import {conectarMongoDB} from '../../middlewares/conectarMongoDB';
+import type {NextApiRequest, NextApiResponse} from 'next';
+import { conectarMongoDB } from '../../middlewares/conectarMongoDB';
 import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
 import type {LoginResposta} from '../../types/LoginResposta';
-import {UsuarioModel} from '../../models/UsuarioModels';
 import md5 from 'md5';
+import { UsuarioModel } from '../../models/UsuarioModels';
 import jwt from 'jsonwebtoken';
 
 const endpointLogin = async (
@@ -13,24 +13,23 @@ const endpointLogin = async (
 
     const {MINHA_CHAVE_JWT} = process.env;
     if(!MINHA_CHAVE_JWT){
-        res.status(500).json({erro : 'ENV jwt nao informada'});
+        return res.status(500).json({erro : 'ENV Jwt nao informada'});
     }
 
-        if(req.method === 'POST'){
-            const {login, senha} = req.body;
+    if(req.method === 'POST'){
+        const {login, senha} = req.body;
 
-            const usuariosEncontrados = await UsuarioModel.find({email : login, senha : md5(senha)});
-            if(usuariosEncontrados && usuariosEncontrados.length > 0){
-                const usuarioEncontrado = usuariosEncontrados[0];
+        const usuariosEncontrados = await UsuarioModel.find({email : login, senha : md5(senha)});
+        if(usuariosEncontrados && usuariosEncontrados.length > 0){
+            const usuarioEncotrado = usuariosEncontrados[0];
 
-                const token = jwt.sign({_id : usuarioEncontrado.id}, MINHA_CHAVE_JWT);
+            const token = jwt.sign({_id : usuarioEncotrado._id}, MINHA_CHAVE_JWT);
 
-                return res.status(200).json({nome : usuarioEncontrado.nome, email : usuarioEncontrado.email, token});
-            }
-
-            return res.status(400).json({erro : 'Usuario ou senha nao encontrado'});
+            return res.status(200).json({nome : usuarioEncotrado.nome, email : usuarioEncotrado.email, token});
         }
-        return res.status(405).json({erro: 'Metodo informado e invalido'});
+        return res.status(400).json({erro : 'Usuario ou senha nao encontrado'});
+    }
+    return res.status(405).json({erro : 'Metodo informado nao e valido'});
 }
 
 export default conectarMongoDB(endpointLogin);
