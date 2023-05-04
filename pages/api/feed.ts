e1d2bf750fb7f62b846a9f1647f3218ebcec7f82
Ana Cristina oliveira <121919367+GilAlvesOliveira@users.noreply.github.com>
2023-05-04 13:01:15 -0300

    Update feed.ts

--- pages/api/feed.ts
+++ pages/api/feed.ts
@@ -2,9 +2,9 @@ import type {NextApiRequest, NextApiResponse} from 'next';
 import type {RespostaPadraoMsg} from '../../types/RespostaPadraoMsg';
 import {validarTokenJWT} from '../../middlewares/validarTokenJWT';
 import {conectarMongoDB} from '../../middlewares/conectarMongoDB';
-import { UsuarioModel } from '@/models/UsuarioModels';
-import { PublicacaoModel } from '@/models/PublicacaoModels';
-import publicacao from './publicacao';
+import {UsuarioModel} from '../../models/UsuarioModels';
+import {PublicacaoModel } from '../../models/PublicacaoModels';
+import {SeguidorModel} from '../../models/SeguidorModel';
 
 const feedEndpoint = async (req : NextApiRequest, res : NextApiResponse<RespostaPadraoMsg | any>) => {
     try{
@@ -12,16 +12,48 @@ const feedEndpoint = async (req : NextApiRequest, res : NextApiResponse<Resposta
             if(req?.query?.id){
                 const usuario = await UsuarioModel.findById(req?.query?.id);
                 if(!usuario){
-                    return res.status(400).json({erro : 'Usuario nao encontrado'})
+                    return res.status(400).json({erro : 'Usuario nao encontrado'});
                 }
+
                 const publicacoes = await PublicacaoModel
-                .find({idUsuario : usuario._id})
+                    .find({idUsuario : usuario._id})
+                    .sort({data : -1});
+
+                return res.status(200).json(publicacoes);
+            }else{
+                const {userId} = req.query;
+                const usuarioLogado = await UsuarioModel.findById(userId);
+                if(!usuarioLogado){
+                    return res.status(400).json({erro : 'Usuario nao encontrado'});
+                }
+
+                const seguidores = await SeguidorModel.find({usuarioId : usuarioLogado._id});
+                const seguidoresIds = seguidores.map(s => s.usuarioSeguidoId);
+
+                const publicacoes = await PublicacaoModel.find({
+                    $or : [
+                        {idUsuario : usuarioLogado._id},
+                        {idUsuario : seguidoresIds}
+                    ]
+                })
                 .sort({data : -1});
 
-            return res.status(200).json(publicacoes);
+                const result = [];
+                for (const publicacao of publicacoes) {
+                   const usuarioDaPublicacao = await UsuarioModel.findById(publicacao.idUsuario);
+                   if(usuarioDaPublicacao){
+                        const final = {...publicacao._doc, usuario : {
+                            nome : usuarioDaPublicacao.nome,
+                            avatar : usuarioDaPublicacao.avatar
+                        }};
+                        result.push(final);  
+                   }
+                }
+
+                return res.status(200).json(result);
             }
         }
-        return res.status(405).json({erro : 'Metado informado nao e valido'})
+        return res.status(405).json({erro : 'Metodo informado nao e valido'});
     }catch(e){
         console.log(e);
     }
